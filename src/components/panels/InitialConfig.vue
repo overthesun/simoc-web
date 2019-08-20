6159bff6429ecc6953d34cb7507c4b187342dac1
Ezio Melotti <ezio.melotti@gmail.com>
2019-08-20 15:28:52 +0200

    Rename and expand the "Initial Configuration" panel.

--- src/components/panels/InitialConfig.vue
+++ src/components/panels/InitialConfig.vue
@@ -10,10 +10,14 @@
         <div class='line-item'>{{getConfiguration.food.amount}}</div>
         <div class='line-title'>Crew Quarters:</div>
         <div class='line-item'>{{stringFormatter(getConfiguration.crewQuarters.type)}}</div>
-        <div class='line-title'>ECLSS:</div>
-        <div class='line-item'>{{getConfiguration.eclss.amount}}</div>
         <div class='line-title'>Greenhouse:</div>
         <div class='line-item'>{{stringFormatter(getConfiguration.greenhouse.type)}}</div>
+        <div class='line-title'>Solar PV Array:</div>
+        <div class='line-item'>{{getConfiguration.powerGeneration.amount}}</div>
+        <div class='line-title'>Batteries:</div>
+        <div class='line-item'>{{getConfiguration.powerStorage.amount}}</div>
+        <div class='line-title'>ECLSS:</div>
+        <div class='line-item'>{{getConfiguration.eclss.amount}}</div>
     </div>
 </template>
 
@@ -22,7 +26,7 @@ import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
 import {StringFormatter} from '../../javascript/stringFormatter'
 
 export default {
-    panelTitle: 'Mission Configuration',
+    panelTitle: 'Initial Configuration',
     computed:{
         ...mapGetters('wizard',['getConfiguration']),
         ...mapGetters('dashboard',['getAirStorageRatio','getTotalAgentMass','getAgentType']),
