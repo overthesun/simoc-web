a418fa303b65d4f741a9a9240948e0f1f9b20b2e
Ezio Melotti <ezio.melotti@gmail.com>
2019-09-26 08:25:43 +0200

    Add a top bar to the dashboard.

---
+++ src/components/bars/index.js
@@ -0,0 +1,1 @@
+export {default as TheTopBar} from './TheTopBar'
--- src/components/base/BaseDashboard.vue
+++ src/components/base/BaseDashboard.vue
@@ -6,17 +6,7 @@ future dashboard views
     <div id='dashboard-wrapper'>
         <!-- Show the dashboard menu component when getMenuActive is true. -->
         <MainMenu v-if="getMenuActive" />
-        <section id='header-wrapper'>
-            <div id='logo-wrapper'>
-                <img src='../../assets/simoc-logo.svg' class='simoc-logo'/>
-                <span class='simoc-logo-title'>SIMOC</span>
-            </div>
-            <div id='dashboard-menu-icon'>
-                <span @click='SETMENUACTIVE(true)' title='Open menu'>
-                    <fa-icon class='fa-icon menu-icon' :icon="['fas','bars']"/>
-                </span>
-            </div>
-        </section>
+        <TheTopBar />
         <section class='main-wrapper'>
             <Main />
         </section>
@@ -31,9 +21,11 @@ future dashboard views
 
 <script>
 import {Timeline,PlayButton,StepControls,SpeedControls,Main,MainMenu} from '../../components/dashboard'
+import {TheTopBar} from '../../components/bars'
 import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
 export default {
     components:{
+        "TheTopBar": TheTopBar,
         "PlayButton": PlayButton,
         "Timeline": Timeline,
         "StepControls": StepControls,
@@ -62,37 +54,6 @@ export default {
     grid-template-rows: 50px minmax(0px,1fr) 50px;
 }
 
-#header-wrapper {
-    display: grid;
-    grid-template-columns: minmax(0px,1fr) 50px;
-    background: linear-gradient(#333, #444);
-    box-shadow: 0 5px 5px #3339;
-    padding: 0 16px;
-}
-#logo-wrapper {
-    display: flex;
-    align-items: center;
-    font-family: "Nasalization", "Open Sans", sans-serif;
-    font-weight: 400;
-    font-size: 32px;
-}
-#logo-wrapper img {
-    height: 40px;
-    margin-right: 16px;
-}
-
-#dashboard-menu-icon {
-    display: flex;
-    justify-content: right;
-    align-items: center;
-    box-sizing: border-box;
-    font-size: 36px;
-    width: 50px;
-    height: 50px;
-}
-#dashboard-menu-icon span:hover {
-    font-size: 40px;
-}
 #footer-wrapper {
     position: relative;
     font-size: 18px;
