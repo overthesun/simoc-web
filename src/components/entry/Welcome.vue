a47cbc910d010be381ae6613255dc9ea84399162
Ezio Melotti <ezio.melotti@gmail.com>
2020-04-17 05:28:59 +0200

    Update the Welcome screen.

--- src/components/entry/Welcome.vue
+++ src/components/entry/Welcome.vue
@@ -6,25 +6,39 @@
 <template>
     <div class='entry-wrapper'>
         <BaseEntry>
-            <template v-slot:option-items>
-                <div class='option-item option-item-active'>
-                    SIMOC BETA
-                </div>
-            </template>
-
             <template v-slot:entry-main>
                 <div class='welcome-wrapper'>
-                    <p class='welcome-title'>WELCOME TO THE SIMOC BETA</p>
+                    <p class='welcome-title'>WELCOME TO SIMOC</p>
 
-                    <p>Please keep in mind the following while using this closed ecosystem simulation.</p>
+                    <p>SIMOC is a Scalable, Interactive Model of an Off-world Community.</p>
 
-                    <p>No personal information will be stored or used by SIMOC services. All login information is
-                    required simply for the creation of each user session, tracking of user settings, and session data.
-                    Server wipes may occur during the course of this private beta, including user login information.</p>
+                    <p>Through the SIMOC web interface you will configure and then test a Mars
+                    habitat of your design. You will select food rations, life support,
+                    solar panels and batteries, crew quarters and greenhouse, and plants to
+                    clean the air and provide food. Set your model in motion and learn if
+                    your astronauts make it ... or not! As an iterative research tool, SIMOC
+                    encourages you to make adjustments and try again until you find the
+                    right combination to sustain your mission on Mars.</p>
 
-                    <p>This product remains in development, therefore not all functionality may be present. If you experience a bug while using SIMOC, please submit a report to bugs@simoc.space with a complete description, including what you were doing prior to the issue.</p>
+                    <p>In using SIMOC, no personal information will be stored or used by SIMOC
+                    or its developer Over the Sun, LLC. Login information is required only
+                    for the creation of user sessions, storing user settings, and holding
+                    session data. The server may be reset from time to time, so be certain
+                    to download your preferred configurations and simulation data.</p>
 
-                    <p>For further details and explanations please visit: <a class='link' href="https://simoc.space/">www.simoc.space</a></p>
+                    <p>If you experience a bug while using SIMOC, please submit a report to
+                    <a class='link' href='mailto:bugs@simoc.space?subject=SIMOC%20Bug%3A%20'>
+                    bugs@simoc.space</a> with a complete description, including what you were
+                    doing prior to the issue.</p>
+
+                    <p>To learn more about SIMOC, enjoy tutorials, and download classroom
+                    curricula visit: <a class='link' href="https://simoc.space/">
+                    www.simoc.space</a></p>
+
+                    <p>Finally, by pressing the PROCEED button below, you confirm that you have
+                    read and agree to the
+                    <a class='link' href="https://simoc.space/about/end-user-license-agreement/">
+                    End User License Agreement</a>.</p>
                 </div>
             </template>
 
@@ -66,8 +80,8 @@ export default {
 }
 
 .welcome-title {
-    text-align: center;
     margin-top: 0;
+    font-weight: bold;
 }
 
 p {
