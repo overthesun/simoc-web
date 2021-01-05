4f2884f7b63e176d29ffadb442987fc769acfbdf
Ezio Melotti <ezio.melotti@gmail.com>
2021-01-05 08:32:22 +0100

    Add back the days/hours tooltip in the timeline.

--- src/components/dashboard/StepControls.vue
+++ src/components/dashboard/StepControls.vue
@@ -6,7 +6,7 @@
         <span class='icon-wrapper' @click='prevStep' title='Previous step'>
             <fa-icon class='fa-icon' :icon="['fas','step-backward']"/>
         </span>
-        <span>{{getCurrentStepBuffer}}/{{getTotalMissionHours}}</span>
+        <span :title="formatHours(getCurrentStepBuffer-1) + '/' + formatHours(getTotalMissionHours)">{{getCurrentStepBuffer}}/{{getTotalMissionHours}}</span>
         <span class='icon-wrapper' @click='nextStep' title='Next step'>
             <fa-icon class='fa-icon' :icon="['fas','step-forward']"/>
         </span>
@@ -27,6 +27,12 @@ export default {
         },
         nextStep: function() {
             this.UPDATEBUFFERCURRENT(this.getCurrentStepBuffer+1)
+        },
+        // the step starts at 1, so make sure to pass getCurrentStepBuffer-1
+        formatHours: function (totalHours) {
+            let days = Math.floor(totalHours / 24)
+            let hours = totalHours % 24
+            return `${days}d ${hours}h`
         },
     },
 }
