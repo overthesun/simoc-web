4aa82fa22723135711574a1f74628d7e941eed70
Ezio Melotti <ezio.melotti@gmail.com>
2021-07-21 20:50:57 +0200

    Always check for new simdata files in <run.sh>.

--- run.sh
+++ run.sh
@@ -8,15 +8,19 @@ if [ ! -e node_modules ]; then
 fi
 
 if [ ! -e src/assets/simdata ]; then
-    echo '* No simdata dir found, downloading presets simdata...'
+    echo '* No simdata dir found, creating <src/assets/simdata> dir...'
     cd src/assets
     mkdir simdata
-    cd simdata
-    wget -r -nH --cut-dirs=2 --no-parent --accept="*.json" -erobots=off -q --show-progress https://simoc.space/download/simdata/
-    echo '* Simulation data downloaded to src/assets/simdata'
-    cd ../../..
+    cd ../..
+    echo '* <src/assets/simdata> dir created'
 fi
 
+echo '* Updating/downloading presets simdata...'
+cd src/assets/simdata
+wget -c -r -nH --cut-dirs=2 --no-parent --accept="*.json" -erobots=off -q https://simoc.space/download/simdata/
+echo '* Presets simdata downloaded to <src/assets/simdata>'
+cd ../../..
 
-echo '* Opening shell, to start the dev server use: npm run serve'
+
+echo '* To start the dev server use: npm run serve'
 /bin/bash
