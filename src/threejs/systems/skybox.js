8b87c6e55f2a2b42dbc2cde446046a69086d9dd6
Grant <granthawkins88@gmail.com>
2021-08-29 06:43:37 +0700

    add new ground texture

--- src/threejs/systems/skybox.js
+++ src/threejs/systems/skybox.js
@@ -4,11 +4,11 @@ const buildSkybox = async (scene, settings) => {
     const angles = ['ft', 'bk', 'up', 'dn', 'rt', 'lf']
     const images = {}
     // ref: https://stackoverflow.com/a/37576787
-    await Promise.all(angles.map(async angle => {
+    await Promise.all([...angles, 'ground'].map(async angle => {
         // image source: https://opengameart.org/content/mayhems-skyboxes-more
         // ref: (acdcjunior's answer) https://stackoverflow.com/questions/47313165/how-to-reference-static-assets-within-vue-javascript
         // eslint-disable-next-line prefer-template
-        const image = await import('../../assets/skybox/h2s_' + angle + '.jpg')
+        const image = await import('../../assets/skybox/' + angle + '.jpg')
         images[angle] = image.default
     }))
 
@@ -18,10 +18,10 @@ const buildSkybox = async (scene, settings) => {
             .load(angles.map(angle => images[angle]))
 
     // Add the ground
-    const groundTexture = new THREE.TextureLoader().load(images.dn)
+    const groundTexture = new THREE.TextureLoader().load(images.ground)
     groundTexture.wrapS = THREE.RepeatWrapping
     groundTexture.wrapT = THREE.RepeatWrapping
-    groundTexture.repeat.set(100, 100)
+    groundTexture.repeat.set(20, 20)
     const groundGeometry = new THREE.PlaneGeometry(10000, 10000)
     const groundMaterial = new THREE.MeshStandardMaterial({map: groundTexture})
     const mesh = new THREE.Mesh(groundGeometry, groundMaterial)
