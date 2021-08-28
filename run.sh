b29278cc078c7e73fe92ce716c29f7433d70a5af
Grant <granthawkins88@gmail.com>
2021-08-28 09:16:11 +0700

    add script to download models, add directory to .gitignore

--- run.sh
+++ run.sh
@@ -21,6 +21,33 @@ wget -c -r -nH --cut-dirs=2 --no-parent --accept="*.json" -erobots=off -q https:
 echo '* Presets simdata downloaded to <src/assets/simdata>'
 cd ../../..
 
+if [ ! -e src/assets/models ]; then
+    echo '* No models dir found, creating <src/assets/models> dir...'
+    cd src/assets
+    mkdir models
+    cd ../..
+    echo '* <src/assets/models> dir created'
+fi
+
+echo '* Updating/downloading 3D models...'
+cd src/assets/models
+wget -c -r -nH --cut-dirs=2 --no-parent --accept="*.glb" -erobots=off -q https://simoc.space/download/models/
+echo '* 3D models downloaded to <src/assets/models>'
+cd ../../..
+
+if [ ! -e src/assets/skybox ]; then
+    echo '* No skybox dir found, creating <src/assets/skybox> dir...'
+    cd src/assets
+    mkdir skybox
+    cd ../..
+    echo '* <src/assets/skybox> dir created'
+fi
+
+echo '* Updating/downloading Skybox images...'
+cd src/assets/skybox
+wget -c -r -nH --cut-dirs=2 --no-parent --accept="*.jpg" -erobots=off -q https://simoc.space/download/skybox/
+echo '* Skybox images downloaded to <src/assets/skybox>'
+cd ../../..
 
 echo '* To start the dev server use: npm run serve'
 /bin/bash
