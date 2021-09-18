092e35f6f965d163100ce93e7473995006fa3a86
Grant <granthawkins88@gmail.com>
2021-09-18 18:11:30 +0700

    limit camera from panning underground

--- src/threejs/systems/controls.js
+++ src/threejs/systems/controls.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const buildControls = (camera, domElement, settings, addTick) => {
@@ -7,6 +8,21 @@ const buildControls = (camera, domElement, settings, addTick) => {
     controls.maxDistance = settings.maxDistance ? settings.maxDistance : 200
     controls.enableDamping = settings.enableDamping
     controls.autoRotate = settings.rotate
+
+    // Limit pan
+    // eslint-disable-next-line max-len
+    // ref: https://discourse.threejs.org/t/how-to-limit-pan-in-orbitcontrols-for-orthographiccamera/9061/10
+    const minPan = new THREE.Vector3(-50, 0, -50)
+    const maxPan = new THREE.Vector3(50, 0, 50)
+    const adjPan = new THREE.Vector3()
+
+    controls.addEventListener('change', () => {
+        adjPan.copy(controls.target)
+        controls.target.clamp(minPan, maxPan)
+        adjPan.sub(controls.target)
+        camera.position.sub(adjPan)
+    })
+    // --
 
     addTick({
         name: 'controls',
