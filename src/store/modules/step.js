cee4618a10ff68247afbc9679f52ded5d9dc24b2
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-12-28 14:08:20 -0800

    Build storage_capacities in a getter

--- src/store/modules/step.js
+++ src/store/modules/step.js
@@ -20,6 +20,7 @@
 export default {
     // TODO: Remove unnecessary state variables and their associated setters and getters
     state: {
+        // mission options and metadata state variables
         id: 0, // Might not need
         stepNum: 0,
         userId: 0, // Might not need
@@ -29,16 +30,48 @@ export default {
         hoursPerStep: 1,
         isTerminated: false, // Might not need
         terminationReason: null, // Might not need
+
+        // containers for atomic state variables
         agentGrowth: {}, // Might not need
         totalAgentCount: {},
         totalProduction: {},
         totalConsumption: {},
         detailsPerAgent: {},
         storageCapacities: {},
+
+        // air_storage atomic state variables
+        atmoO2: {value: 0, unit: 'kg'},
+        atmoCO2: {value: 0, unit: 'kg'},
+        atmoN2: {value: 0, unit: 'kg'},
+        atmoCH4: {value: 0, unit: 'kg'},
+        atmoH2: {value: 0, unit: 'kg'},
+        atmoH2O: {value: 0, unit: 'kg'},
+
+        // water_storage atomic state variables
+        h2oPotb: {value: 0, unit: 'kg'},
+        h2oUrin: {value: 0, unit: 'kg'},
+        h2oWste: {value: 0, unit: 'kg'},
+        h2oTret: {value: 0, unit: 'kg'},
+
+        // nutrient_storage atomic state variables
+        biomassTotl: {value: 0, unit: 'kg'},
+        soldN: {value: 0, unit: 'kg'},
+        soldP: {value: 0, unit: 'kg'},
+        soldK: {value: 0, unit: 'kg'},
+        soldWste: {value: 0, unit: 'kg'},
+
+        // power_storage atomic state variables
+        enrgKwh: {value: 0, unit: 'kWh'},
+
+        // food_storage atomic state variables
+        foodEdbl: {value: 0, unit: 'kg'},
+
+        // step_data of atomic state variable containers
         stepData: {},
         stepBatch: [],
     },
     getters: {
+        // mission options and metadata state variable getters
         getId: state => state.id,
         getStepNum: state => state.stepNum,
         getUserId: state => state.userId,
@@ -48,13 +81,64 @@ export default {
         getHoursPerStep: state => state.hoursPerStep,
         getIsTerminated: state => state.isTerminated,
         getTerminationReason: state => state.terminationReason,
+
+        // atomic container getters returns dictionaries in proper format
         getAgentGrowth: state => state.agentGrowth,
         getTotalAgentCount: state => state.totalAgentCount,
         getTotalProduction: state => state.totalProduction,
         getTotalConsumption: state => state.totalConsumption,
         getDetailsPerAgent: state => state.detailsPerAgent,
-        getStorageCapacities: state => state.storageCapacities,
-        getStepData(state) {
+        getStorageCapacities(state) {
+            return {
+                air_storage: {
+                    1: {
+                        atmo_o2: state.atmoO2,
+                        atmo_co2: state.atmoCO2,
+                        atmo_n2: state.atmoN2,
+                        atmo_ch4: state.atmoCH4,
+                        atmo_h2: state.atmoH2,
+                        atmo_h2o: state.atmoH2O,
+                    },
+                },
+                water_storage: {
+                    1: {
+                        h2o_potb: state.h2oPotb,
+                        h2o_urin: state.h2oUrin,
+                        h2o_wste: state.h2oWste,
+                        h2o_tret: state.h2oTret,
+                    },
+                },
+                nutrient_storage: {
+                    1: {
+                        biomass_totl: state.biomassTotl,
+                        sold_n: state.soldN,
+                        sold_p: state.soldP,
+                        sold_k: state.soldK,
+                        sold_wste: state.soldWste,
+                    },
+                },
+                power_storage: {
+                    1: {
+                        enrg_kwh: state.enrgKwh,
+                    },
+                },
+                food_storage: {
+                    1: {
+                        food_edbl: state.foodEdbl,
+                    },
+                },
+            }
+        },
+
+        // atomic state getters
+        getAtmoO2: state => state.atmoO2,
+        getAtmoCO2: state => state.atmoCO2,
+        getAtmoN2: state => state.atmoN2,
+        getAtmoCH4: state => state.atmoCH4,
+        getAtmoH2: state => state.atmoH2,
+        getAtmoH2O: state => state.atmoH2O,
+
+        getStepData(state, getters) {
             return {
                 id: state.id,
                 step_num: state.stepNum,
@@ -70,12 +154,13 @@ export default {
                 total_production: state.totalProduction,
                 total_consumption: state.totalConsumption,
                 details_per_agent: state.detailsPerAgent,
-                storage_capacities: state.storageCapacities,
+                storage_capacities: getters.getStorageCapacities,
             }
         },
         getStepBatch: state => state.stepBatch,
     },
     mutations: {
+        // mission options and metadata state variable mutators
         setId(state, value) {
             state.id = value
         },
@@ -103,6 +188,8 @@ export default {
         setTerminationReason(state, value) {
             state.terminationReason = value
         },
+
+        // atomic state variable mutators
         setAgentGrowth(state, value) {
             state.agentGrowth = value
         },
@@ -221,117 +308,120 @@ export default {
 
             state.detailsPerAgent = details_per_agent
         },
-        setStorageCapacities(state, value) {
-            const storage_capacities = {
-                air_storage: {
-                    1: {
-                        atmo_o2: {
-                            // value: value.atmo_o2,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        atmo_co2: {
-                            // value: value.atmo_co2,
-                            value: value,
-                            unit: 'kg',
-                        },
-                        atmo_n2: {
-                            // value: value.atmo_n2,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        atmo_ch4: {
-                            // value: value.atmo_ch4,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        atmo_h2: {
-                            // value: value.atmo_h2,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        atmo_h2o: {
-                            // value: value.atmo_h2o,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                    },
-                },
-                water_storage: {
-                    1: {
-                        h2o_potb: {
-                            // value: value.h2o_potb,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        h2o_urin: {
-                            // value: value.h2o_urin,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        h2o_wste: {
-                            // value: value.h2o_wste,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        h2o_tret: {
-                            // value: value.h2o_tret,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                    },
-                },
-                nutrient_storage: {
-                    1: {
-                        biomass_totl: {
-                            // value: value.biomass_totl,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        sold_n: {
-                            // value: value.sold_n,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        sold_p: {
-                            // value: value.sold_p,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        sold_k: {
-                            // value: value.sold_k,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                        sold_wste: {
-                            // value: value.sold_wste,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                    },
-                },
-                power_storage: {
-                    1: {
-                        enrg_kwh: {
-                            // value: value.enrg_kwh,
-                            value: 0,
-                            unit: 'kWh',
-                        },
-                    },
-                },
-                food_storage: {
-                    1: {
-                        food_edbl: {
-                            // value: value.food_edbl,
-                            value: 0,
-                            unit: 'kg',
-                        },
-                    },
-                },
-            }
 
-            state.storageCapacities = storage_capacities
+        // air_storage atomic state variable mutators
+        setAtmoO2(state, value) {
+            state.atmoO2 = {
+                value: value,
+                unit: 'kg',
+            }
         },
+        setAtmoCO2(state, value) {
+            state.atmoCO2 = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setAtmoN2(state, value) {
+            state.atmoN2 = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setAtmoCH4(state, value) {
+            state.atmoCH4 = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setAtmoH2(state, value) {
+            state.atmoH2 = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setAtmoH2O(state, value) {
+            state.atmoH2O = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+
+        // water_storage atomic state variable mutators
+        setH2OPotb(state, value) {
+            state.h2oPotb = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setH2OUrin(state, value) {
+            state.h2oUrin = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setH2OWste(state, value) {
+            state.h2oWste = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setH2OTret(state, value) {
+            state.h2oTret = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+
+        // nutrient_storage atomic state variable mutators
+        setBiomassTotl(state, value) {
+            state.biomassTotl = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setSoldN(state, value) {
+            state.soldN = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setSoldP(state, value) {
+            state.soldP = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setSoldK(state, value) {
+            state.soldK = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+        setSoldWste(state, value) {
+            state.soldWste = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+
+        // power_storage atomic state variable mutators
+        setEnrgKwh(state, value) {
+            state.enrgKwh = {
+                value: value,
+                unit: 'kWh',
+            }
+        },
+
+        // food_storage atomic state variable mutators
+        setFoodEdbl(state, value) {
+            state.foodEdbl = {
+                value: value,
+                unit: 'kg',
+            }
+        },
+
+        // stepBatch mutator pushes new step_data onto an array
         setStepBatch(state, value) {
             console.log('Adding step data to batch...')
             console.log(value)
@@ -355,17 +445,17 @@ export default {
                             break
                         case 'co2_ppm':
                             // console.log(`co2_ppm[${item.step_num}]:   ${item.co2_ppm}`)
-                            commit('setStorageCapacities', item.co2_ppm)
+                            commit('setAtmoCO2', item.co2_ppm)
                             break
                         case 'hum_perc':
                             // console.log(`hum_perc[${item.step_num}]:  ${item.hum_perc}`)
                             // TODO: Create humidity state
-                            // commit('', item.hum_perc)
+                            // commit('setAtmoHum', item.hum_perc)
                             break
                         case 'temp':
-                            // TODO: Create temperature state
                             // console.log(`temp[${item.step_num}]:      ${item.temp}`)
-                            // commit('', item.temp)
+                            // TODO: Create temperature state
+                            // commit('setAtmoTemp', item.temp)
                             break
                         default:
                             newStepData = false
