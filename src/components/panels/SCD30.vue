0b636fd029d50ba15b721253e1d23f5b63b43f15
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-01-20 19:16:37 -0800

    Add initial SCD-30 panel for live mode setup.

---
+++ src/components/panels/SCD30.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="panel-dl-wrapper">
+        <dl v-if="info_section == 'atmo_readings'">
+            <dt>CO2</dt>
+            <dd>{{getAtmoCO2PPM()}} ppm</dd>
+            <dt>Temperature</dt>
+            <dd>{{getTemp()}} C°</dd>
+            <dt>Humidity</dt>
+            <dd>{{getHumidity()}}%</dd>
+        </dl>
+    </section>
+</template>
+
+
+<script>
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
+import {StringFormatter} from '../../javascript/utils'
+
+export default {
+    panelTitle: 'SCD-30 NDIR Sensor',
+    modes: ['live'],
+    data() {
+        return {
+            info_section: 'atmo_readings',
+        }
+    },
+    computed: {
+        ...mapGetters('live', ['getAtmoCO2', 'getAtmoTemp', 'getAtmoHum']),
+    },
+    methods: {
+        stringFormatter: StringFormatter,
+        getAtmoCO2PPM() {
+            return this.getAtmoCO2.value
+        },
+        getTemp() {
+            return this.getAtmoTemp.value
+        },
+        getHumidity() {
+            return this.getAtmoHum.value
+        },
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+#mission-info-select {
+  width: 50%;
+  margin-bottom: 1em;
+}
+</style>
