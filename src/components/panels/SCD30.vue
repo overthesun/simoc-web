75b952d0b21f95dd9850d1e7422f11eeb570031f
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-01-29 18:00:54 -0800

    Pass stepNum as an argument to the getters in the SCD30 panel.

--- src/components/panels/SCD30.vue
+++ src/components/panels/SCD30.vue
@@ -2,11 +2,11 @@
     <section class="panel-dl-wrapper">
         <dl>
             <dt>CO2</dt>
-            <dd>{{getAtmoCO2.value}} ppm</dd>
+            <dd>{{getAtmoCO2(getStepNum)}} ppm</dd>
             <dt>Temperature</dt>
-            <dd>{{getAtmoTemp.value}} °C</dd>
+            <dd>{{getAtmoTemp(getStepNum)}} °C</dd>
             <dt>Relative Humidity</dt>
-            <dd>{{getAtmoHum.value}}%</dd>
+            <dd>{{getAtmoHum(getStepNum)}}%</dd>
         </dl>
     </section>
 </template>
@@ -19,7 +19,7 @@ export default {
     panelTitle: 'SCD-30 NDIR Sensor',
     modes: ['live'],
     computed: {
-        ...mapGetters('livedata', ['getAtmoCO2', 'getAtmoTemp', 'getAtmoHum']),
+        ...mapGetters('livedata', ['getAtmoCO2', 'getAtmoTemp', 'getAtmoHum', 'getStepNum']),
     },
 }
 </script>
