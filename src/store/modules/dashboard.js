b7695ba642dd7b1392f0259081f69ccd032580b6
Grant <50287275+granawkins@users.noreply.github.com>
2022-03-02 10:43:21 +0700

    fix linting errors in store/dashboard

--- src/store/modules/dashboard.js
+++ src/store/modules/dashboard.js
@@ -29,6 +29,61 @@
 // To be added: Need to add a variable to store the current step interval.
 // This is currently only done locally within the Controls component.
 
+const parse_updated_game_vars = (game_config, currency_desc) => {
+    /*
+    As of October '21, the backend no longer distinguishes between agents and storages.
+    Much of the dashboard was designed to pull directly from gameConfig.storages.
+    Here, we determine which agents have storage, and add them back manually.
+    */
+
+    // Load currency data into dict
+    const currencyDict = {}
+    Object.entries(currency_desc).forEach(([currencyClass, currencies]) => {
+        Object.entries(currencies).forEach(([currency, currencyData]) => {
+            currencyDict[currency] = {...currencyData, currencyClass}
+        })
+    })
+
+    let humanAtmosphere = ''
+    let gameConfig = {}
+    // Load game config data into state
+    const storages = {}
+    const storageTypes = {
+        atmosphere: 'air_storage',
+        food: 'food_storage',
+        water: 'water_storage',
+        nutrients: 'nutrient_storage',
+        energy: 'power_storage',
+    }
+    Object.entries(game_config.agents).forEach(([agent_type, attributes]) => {
+        // Agents can store more than 1 class of currencies; we add a 'storageType'
+        // attribute which is an array of all the storageTypes it contains.
+        const storageType = []
+        Object.keys(attributes).forEach(field => {
+            if (Object.keys(currencyDict).includes(field)) {
+                const {currencyClass} = currencyDict[field]
+                const storage = storageTypes[currencyClass]
+                if (!storageType.includes(storage)) {
+                    storageType.push(storage)
+                    if (currencyClass === 'atmosphere' &&
+                            agent_type.includes('habitat')) {
+                        humanAtmosphere = agent_type
+                    }
+                }
+            }
+        })
+        if (storageType.length) {
+            storages[agent_type] = [{storageType, ...attributes}]
+        }
+    })
+    if (!Object.keys(game_config).includes('storages')) {
+        gameConfig = {...game_config, storages}
+    } else {
+        gameConfig = game_config
+    }
+    return {currencyDict, gameConfig, humanAtmosphere}
+}
+
 export default {
     state: {
         // parameters for the get_steps route
@@ -117,7 +172,7 @@ export default {
 
             // TODO: Revert ABM Workaround
             const game_vars = parse_updated_game_vars(simdata.game_config, currency_desc)
-            const {humanAtmosphere,  currencyDict, gameConfig} = game_vars
+            const {humanAtmosphere, currencyDict, gameConfig} = game_vars
             state.humanAtmosphere = humanAtmosphere
             state.currencyDict = currencyDict
             state.gameConfig = gameConfig
@@ -202,7 +257,7 @@ export default {
 
             // TODO: Revert ABM Workaround
             const game_vars = parse_updated_game_vars(game_config, currency_desc)
-            const {humanAtmosphere,  currencyDict, gameConfig} = game_vars
+            const {humanAtmosphere, currencyDict, gameConfig} = game_vars
             state.humanAtmosphere = humanAtmosphere
             state.currencyDict = currencyDict
             state.gameConfig = gameConfig
@@ -372,59 +427,4 @@ export default {
             })
         },
     },
-}
-
-const parse_updated_game_vars = (game_config, currency_desc) => {
-    /*
-    As of October '21, the backend no longer distinguishes between agents and storages.
-    Much of the dashboard was designed to pull directly from gameConfig.storages.
-    Here, we determine which agents have storage, and add them back manually.
-    */
-
-    // Load currency data into dict
-    const currencyDict = {}
-    Object.entries(currency_desc).forEach(([currencyClass, currencies]) => {
-        Object.entries(currencies).forEach(([currency, currencyData]) => {
-            currencyDict[currency] = {...currencyData, currencyClass}
-        })
-    })
-
-    let humanAtmosphere = ''
-    let gameConfig = {}
-    // Load game config data into state
-    const storages = {}
-    const storageTypes = {
-        atmosphere: 'air_storage',
-        food: 'food_storage',
-        water: 'water_storage',
-        nutrients: 'nutrient_storage',
-        energy: 'power_storage',
-    }
-    Object.entries(game_config.agents).forEach(([agent_type, attributes]) => {
-        // Agents can store more than 1 class of currencies; we add a 'storageType'
-        // attribute which is an array of all the storageTypes it contains.
-        const storageType = []
-        Object.keys(attributes).forEach(field => {
-            if (Object.keys(currencyDict).includes(field)) {
-                const {currencyClass} = currencyDict[field]
-                const storage = storageTypes[currencyClass]
-                if (!storageType.includes(storage)) {
-                    storageType.push(storage)
-                    if (currencyClass === 'atmosphere' &&
-                            agent_type.includes('habitat')) {
-                        humanAtmosphere = agent_type
-                    }
-                }
-            }
-        })
-        if (storageType.length) {
-            storages[agent_type] = [{storageType, ...attributes}]
-        }
-    })
-    if (!Object.keys(game_config).includes('storages')) {
-        gameConfig = {...game_config, storages}
-    } else {
-        gameConfig = game_config
-    }
-    return {currencyDict, gameConfig, humanAtmosphere}
 }
