f44bf1b61e15dfa7a34a61fea8a08debaa0ae004
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-03-24 19:38:20 -0700

    Extend PlayButton pauseTimer and resumeTimer to toggle between live and not live in live mode.

--- src/components/dashboard/PlayButton.vue
+++ src/components/dashboard/PlayButton.vue
@@ -21,15 +21,27 @@ export default {
         }
     },
     computed: {
-        ...mapGetters('dashboard', ['getIsTimerRunning']),
+        ...mapGetters('dashboard', ['getIsTimerRunning', 'getCurrentMode']),
     },
     methods: {
-        ...mapMutations('dashboard', ['STARTTIMER', 'PAUSETIMER']),
+        ...mapMutations('dashboard', ['STARTTIMER', 'PAUSETIMER', 'SETSTEPINTERVAL', 'SETISLIVE']),
         pauseTimer() {
+            // If the user selects pause while in the live Dashboard, the Dashboard is no
+            // longer showing live data thus isLive is set to false in the dashboard store.
+            if (this.getCurrentMode === 'live') {
+                this.SETISLIVE(false)
+            }
+
             // pause the timer when the user clicks on the pause button
             this.PAUSETIMER()
         },
         resumeTimer() {
+            // If the user selects play while in the live Dashboard, the stepInterval is
+            // set using the dashboard store mutator SETSTEPINTERVAL to increment the scrubber
+            // at 1000 ms.
+            if (this.getCurrentMode === 'live') {
+                this.SETSTEPINTERVAL(1000)
+            }
             // start/resume the timer when the user clicks on the play button
             this.STARTTIMER()
         },
