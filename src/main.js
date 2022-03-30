f31cec4985b878f6cc9e712bb070da8c374f511b
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-03-29 22:05:49 -0700

    Add circle-dot icon.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import VueGtag from 'vue-gtag-next'
 
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faPause, faPlay, faStepBackward, faStepForward, faMinus, faPlus, faTimes,
-        faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle,
+        faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle, faDotCircle,
         faPlusCircle, faMale, faTrash} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon, FontAwesomeLayers} from '@fortawesome/vue-fontawesome'
 import store from './store/index'
@@ -11,7 +11,7 @@ import router from './router'
 import App from './App'
 
 library.add(faPlay, faPause, faStepBackward, faStepForward, faMinus, faPlus, faTimes,
-            faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle,
+            faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle, faDotCircle,
             faPlusCircle, faMale, faTrash)
 
 createApp(App)
