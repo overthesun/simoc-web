d9bbf9ae5f1aab734ed4086d48a868142c15dfad
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-03-31 15:16:29 -0700

    Add AtmosphericCO2 component to panels directory.

---
+++ src/components/panels/AtmosphericCO2.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="panel-dl-wrapper">
+        <template v-if="getDataBundles.length === 0">
+            <div class="no-data">[Awaiting data...]</div>
+        </template>
+        <template v-else />
+    </section>
+</template>
+
+
+<script>
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
+
+export default {
+    panelTitle: 'Atmospheric CO2',
+    modes: ['live'],
+    computed: {
+        ...mapGetters('dashboard', ['getCurrentStepBuffer']),
+        ...mapGetters('livedata', ['getSensorInfo', 'getReadings', 'getDataBundles']),
+    },
+    methods: {
+
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
