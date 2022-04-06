fd0d1fcc1696b1cde8c9b8a560dd28e82af0e016
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-04-05 20:08:38 -0700

    Add location and unit select elements and default options.

--- src/components/panels/AtmosphericCO2.vue
+++ src/components/panels/AtmosphericCO2.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="panel-graph">
+        <select v-model="location" required>
+            <option :selected="location === 'crew_quarters'" value="crew_quarters">Crew Quarters</option>
+            <!-- <option :selected="location === 'greenhouse'" value="greenhouse">Greenhouse</option>-->
+        </select>
         <div>
-            <AveragesGraph :id="'line-chart-co2'" :plotted-value="'x'" :unit="'ppm'" />
+            <AveragesGraph :id="'canvas-pc-' + canvasNumber" :plotted-value="location" :unit="unit" />
+        </div>
+        <div class="graph-units">
+            <select v-model="unit" required>
+                <option :selected="unit === 'co2_ppm'" value="co2_ppm">ppm</option>
+                <!-- <option :selected="unit === 'co2_ccm'" value="co2_ccm">ccm</option>-->
+            </select>
         </div>
     </div>
 </template>
-
 
 <script>
 import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
