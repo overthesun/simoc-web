13dd08d6ba7a2a1c0ecb02550c2d3a135b5473b5
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-06-19 21:25:18 -0700

    Make the panels enlargeable.

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import VueGtag from 'vue-gtag-next'
 
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faPause, faPlay, faStepBackward, faStepForward, faMinus, faPlus, faTimes,
-        faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle, faDotCircle,
-        faPlusCircle, faMale, faTrash} from '@fortawesome/free-solid-svg-icons'
+        faBars, faArrowsAlt, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle,
+        faDotCircle, faPlusCircle, faMale, faTrash} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon, FontAwesomeLayers} from '@fortawesome/vue-fontawesome'
 import store from './store/index'
 import router from './router'
 import App from './App'
 
 library.add(faPlay, faPause, faStepBackward, faStepForward, faMinus, faPlus, faTimes,
-            faBars, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle, faDotCircle,
-            faPlusCircle, faMale, faTrash)
+            faBars, faArrowsAlt, faArrowLeft, faArrowRight, faInfoCircle, faListUl, faCircle,
+            faDotCircle, faPlusCircle, faMale, faTrash)
 
 createApp(App)
         .use(router)
