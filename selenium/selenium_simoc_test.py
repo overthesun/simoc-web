cbfa276af233d80d09a72f6a754e5f5a77f7b410
IMBCIT <42709847+IMBCIT@users.noreply.github.com>
2022-07-17 18:05:51 -0700

    Apply suggestions from code review
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>

--- selenium/selenium_simoc_test.py
+++ selenium/selenium_simoc_test.py
@@ -1,7 +1,8 @@
-import argparse
-from os import wait
 import time
+import argparse
 import multiprocessing
+
+from os import wait
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -29,19 +30,20 @@ def select_browser(browser):
             #chrome_options.add_argument('--headless')
 
             dc = DesiredCapabilities.CHROME
-            dc['goog:loggingPrefs'] = { 'browser':'ALL' }
+            dc['goog:loggingPrefs'] = {'browser': 'ALL'}
 
-            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options, desired_capabilities=dc)
+            service = ChromeService(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=chrome_options,
+                                      desired_capabilities=dc)
             return driver
         case _:
             print("Browser not currently targeted")
 
 
 def run_sim(browser, days):
-    global select_browser
     driver = select_browser(browser)
 
-    print(multiprocessing.current_process().name+" has started")
+    print(f'{multiprocessing.current_process().name} has started")
 
     driver.get("https://beta.simoc.space")
 
@@ -51,55 +53,61 @@ def run_sim(browser, days):
     driver.find_element(By.ID, 'guest-login').click()
 
     wait = WebDriverWait(driver, 10)
-    sign_in_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="SIGN IN AS GUEST"]')))
+    sign_in_path = '//button[text()="SIGN IN AS GUEST"]'
+    cond = EC.element_to_be_clickable((By.XPATH, sign_in_path))
+    sign_in_button = wait.until(cond)
     sign_in_button.click()
 
     driver.find_element(By.XPATH, '//button[text()="NEW CONFIGURATION"]').click()
 
 
-    sim_options = Select(driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/section[1]/main/form/section/form/label/div[3]/div/select'))
+    presets_path = '//div[contains(@class, 'presets-dropdown')]/select'
+    sim_options = Select(driver.find_element(By.XPATH, presets_path))
     sim_options.select_by_value('sam_one_human_garden')
 
     sim_days = driver.find_element(By.CLASS_NAME, 'input-field-number')
     sim_days.clear()
     sim_days.send_keys(days)
 
-    plant_options = Select(driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/section[1]/main/form/section/div[3]/label[2]/div[3]/select'))
+    plant_path = '//div[contains(@class, 'input-plant-wrapper')]/select'
+    plant_options = Select(driver.find_element(By.XPATH, plant_path))
     plant_options.select_by_value('wheat')
     plant_options.select_by_value('rice')
 
     driver.find_element(By.CLASS_NAME, 'btn-launch').click()
 
-    print("Sim for "+multiprocessing.current_process().name+" has launched")
+    print(f"Sim for {multiprocessing.current_process().name} has launched")
 
     time.sleep(240)
 
     steps = int(days) * 24 
-    message = str(steps)+"/"+str(steps)+" steps sent by the server"
-    found = False
-
-    for log in driver.get_log('browser'):
+    message = f"/{steps} steps sent by the server"
+   for log in driver.get_log('browser'):
         if message in log['message']:
-            print("Sim "+multiprocessing.current_process().name+" was successful")
-            found = True
-        else:
-            continue
-
-    if found is not True:
-        print("Sim "+multiprocessing.current_process().name+ " failed")
+            print("Step message found:", log['message'])
+            if f'{steps}/{steps}' in log['message']:
+                print(f"Sim {multiprocessing.current_process().name} was successful")
+            else:
+                print(f"Sim {multiprocessing.current_process().name} failed")
+            break
+    else:
+        print(f"Sim {multiprocessing.current_process().name} failed")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--processes', '-p', type=int, default=1, help='Number of processes to run')
-    parser.add_argument('--days', '-d', type=str, default='100', help='Number of days for sim to run. Min of 1 and max of 365')
-    parser.add_argument('--browser', '-b', type=str, default='chrome', help='Select browser to run SIMOC in')
+    parser.add_argument('--processes', '-p', type=int, default=1,
+                        help='Number of processes to run')
+    parser.add_argument('--days', '-d', type=str, default='100',
+                        help='Number of days for sim to run. Min of 1 and max of 365')
+    parser.add_argument('--browser', '-b', type=str, default='chrome',
+                        help='Select browser to run SIMOC in')
 
     args = parser.parse_args()
-
-    process = [multiprocessing.Process(target=run_sim, kwargs={'browser':args.browser,'days':args.days}) for _ in range(args.processes)]
-
-    for p in process:
+    kwargs = {'browser': args.browser, 'days': args.days}
+    processes = [multiprocessing.Process(target=run_sim, kwargs=kwargs)
+                 for _ in range(args.processes)]
+    for p in processes:
         p.start()
 
 
