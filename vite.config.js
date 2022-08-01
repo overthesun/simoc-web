fad93849c41b014a95efc77b21f00ae2e15cfe5a
Ian Castellanos <iancastellanos@Ians-MacBook-Pro.local>
2022-07-31 22:29:57 -0700

    removed custom middleware function

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,6 @@ import vue from '@vitejs/plugin-vue';
 import postcssNesting from 'postcss-nesting';
 import path from 'path';
 
-// unnecessary
-const proxyRequests = () => ({
-        name: 'configure-proxy',
-        configureServer(server) {
-            return () => {
-                server.middlewares.use(
-                    '/',
-                    createProxyMiddleware({
-                        target: 'http://nginx:8000',
-                        changeOrigin: true,
-                    })
-                )
-            }
-        }
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
     assetsInclude: ['**/*.glb'],
