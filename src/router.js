c056dbd6f6417193ed79e803f59806e445d9c17a
Ezio Melotti <ezio.melotti@gmail.com>
2022-09-25 20:56:16 +0200

    Avoid using vars (`@`) in the router.
    Apparently vars interfere with lazy loading.
    See https://stackoverflow.com/a/70133246

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
-import EntryView from '@/views/EntryView.vue'
+import EntryView from '/src/views/EntryView.vue'
 
 export default createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,52 +12,56 @@ export default createRouter({
                 {
                     path: '',
                     name: 'initial',
-                    component: () => import('@/components/entry/Welcome.vue'),
+                    component: () => import('/src/components/entry/Welcome.vue'),
                 },
                 {
                     path: 'entry',
                     name: 'entry',
-                    component: () => import('@/components/entry/Login.vue'),
+                    component: () => import('/src/components/entry/Login.vue'),
                 },
                 {
                     path: 'menu',
                     name: 'menu',
-                    component: () => import('@/components/entry/Menu.vue'),
+                    component: () => import('/src/components/entry/Menu.vue'),
                 },
+                /* Menu to select guided/custom config -- no longer used
                 {
                     path: 'menuconfig',
                     name: 'menuconfig',
-                    component: () => import('@/components/entry/MenuConfig.vue'),
+                    component: () => import('/src/components/entry/MenuConfig.vue'),
                 },
+                */
             ],
         },
+        /* Menu to select guided/custom config -- no longer used
         {
             path: '/menuconfig',
             name: 'menuconfig',
-            component: () => import('@/components/entry/MenuConfig.vue'),
+            component: () => import('/src/components/entry/MenuConfig.vue'),
         },
-        {
-            path: '/ace',
-            name: 'ace',
-            conponent: () => import('@/views/AceView.vue'),
-        },
+        */
         {
             path: '/configuration',
             name: 'configuration',
-            component: () => import('@/views/ConfigurationView.vue'),
+            component: () => import('/src/views/ConfigurationView.vue'),
             children: [{
                 path: '/configuration',
-                component: () => import('@/components/base/BaseConfiguration.vue'),
+                component: () => import('/src/components/base/BaseConfiguration.vue'),
             }],
         },
         {
             path: '/dashboard',
             name: 'dashboard',
-            component: () => import('@/views/DashboardView.vue'),
+            component: () => import('/src/views/DashboardView.vue'),
             children: [{
                 path: '/dashboard',
-                component: () => import('@/components/base/BaseDashboard.vue'),
+                component: () => import('/src/components/base/BaseDashboard.vue'),
             }],
+        },
+        {
+            path: '/ace',
+            name: 'ace',
+            conponent: () => import('/src/views/AceView.vue'),
         },
     ],
 })
