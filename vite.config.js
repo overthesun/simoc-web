6d2261e51c0ee36009ab95d81c029380c327d88d
Ezio Melotti <ezio.melotti@gmail.com>
2022-10-12 16:57:05 +0200

    Explicitly list non-nginx paths.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import eslint from 'vite-plugin-eslint'
 import postcssNesting from 'postcss-nesting';
 import path from 'path';
 
+
+// This regex lists the paths that should NOT go to nginx, including
+//   $: i.e. empty location, or simply localhost:8080/
+//   @: some vite-related paths starts with @
+//   src: the frontend files in src/
+//   node_modules: self explanatory
+//   favicon.ico: self explanatory
+//   *: the rest are the routers defined in router.js
+// These paths should go to vite, everything else will go to nginx.
+const nginx_paths = ('^/(?!$|@|src|node_modules|favicon\.ico|' +
+                     'entry|menu|menuconfig|ace|configuration|dashboard)')
+
 // https://vitejs.dev/config/
 export default defineConfig({
     assetsInclude: ['**/*.glb'],
@@ -14,12 +26,12 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/*': {
+            [nginx_paths]: {
                 target: 'http://nginx:8000',
                 changeOrigin: true,
                 secure: false,
-            }
-        }
+            },
+        },
     },
     build: {
         assetsDir: 'static',
