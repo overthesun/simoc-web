91916cae7dac9422a987259fd87ae7bb82fbff55
Grant <50287275+granawkins@users.noreply.github.com>
2023-02-07 12:56:50 +0700

    Agent explorer (#334)
    * first stab
    
    * functional
    
    * tidied up
    
    * linter fixes
    
    * update documentation
    
    * use negative number for out-exchanges, format correctly
    
    * allow "false" to propagate thru parseData, e.g. plant.grown
    
    * Apply suggestions from code review
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>
    
    * manual updates from code review
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>
    
    * linter fix
    
    * update AgentGraph for optimized fullscreen
    
    * updates from code review
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>
    
    ---------
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>

--- src/javascript/utils.js
+++ src/javascript/utils.js
@@ -1,6 +1,11 @@
 // Misc util functions
 
 export function StringFormatter(value) {
+    // special case
+    if (value.toLowerCase() === 'kwh') {
+        return 'kWh'
+    }
+
     // replace underscores with spaces and capitalizes the first letter of each word
     // e.g. 'food_ration' -> 'Food Ration'
     let formatted = ''
@@ -18,6 +23,13 @@ export function ensure_within(value, min, max) {
     // ensure that min <= value <= max, return 0 if value is not a number
     return Math.max(min, Math.min(max, parseInt(value, 10) || 0))
 }
+
+export const colors = [
+    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0',
+    '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8',
+    '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff',
+    '#000000',
+]  // A long arbitrary list of colors, used by GreenhouseDoughnut and AgentExplorer
 
 export function make_labels(start, end, nsteps) {
     // create nsteps labels from start to end, possibly adding empty slots at the beginning
