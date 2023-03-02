e988dbcd9b9460408d82b42528f3c3365a5ca14d
Ezio Melotti <ezio.melotti@gmail.com>
2023-03-02 09:47:26 +0800

    Set `terminate = true` when loading simdata.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -156,11 +156,13 @@ export default {
         if (this.currentMode === 'live') {
             console.log('Starting live dashboard')
             this.maxStepBuffer = 0  // Reset the max buffer value
-            this.initBundleNum = null // Reset initBundleNum to null
+            this.initBundleNum = null  // Reset initBundleNum to null
             this.setupLiveWebsocket()
-        } else if (!this.loadFromSimData) {
-            // if we load the simulation data, there's nothing else to do, otherwise
-            // we have to reset a few more values, init the game, and request steps
+        } else if (this.loadFromSimData) {
+            // if we load simdata, we only need to set terminated to true
+            this.terminated = true
+        } else {
+            // for new sims, reset a few more values, init the game, and request steps
             this.maxStepBuffer = 0  // Reset the max buffer value
             // init a new game, set game id, reset all data buffers
             this.initGame(this.getGameID)
