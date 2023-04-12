d9df8ec0bc21449b72427f85ed118a7d09d87509
Ezio Melotti <ezio.melotti@gmail.com>
2023-04-13 01:27:58 +0800

    Add missing comma.

--- src/components/base/BaseEntry.vue
+++ src/components/base/BaseEntry.vue
@@ -42,7 +42,7 @@ export default {
     data() {
         return {
             env: import.meta.env,
-            is_ngs: window.location.hostname.startsWith('ngs')
+            is_ngs: window.location.hostname.startsWith('ngs'),
         }
     },
 }
