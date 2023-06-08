22f1dd65b875348cf55b83ace44e99eca7603c19
Ezio Melotti <ezio.melotti@gmail.com>
2023-06-08 05:22:38 +0200

    Update NGS UA id to GA4.

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ let tracking_id = null
 if (window.location.hostname.startsWith('beta')) {
     tracking_id = 'G-6M7C80DGTR'  // beta id
 } else {
-    tracking_id = 'UA-29092818-6'  // NGS id
+    tracking_id = 'G-60FL6KJK4M'  // NGS id
 }
 
 createApp(App)
