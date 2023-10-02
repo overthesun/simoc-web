de4e7dbcb9d895cc52c5e182239dbc35910eefd1
IMBCIT <icastellanos@imbctech.com>
2023-10-02 08:59:54 -0700

    fixing linter issues

--- src/test/Welcome.spec.js
+++ src/test/Welcome.spec.js
@@ -1,15 +1,15 @@
-import {mount} from "@vue/test-utils";
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-//https://pinia.vuejs.org/cookbook/testing.html
-import { createTestingPinia } from '@pinia/testing'
-import Welcome from "@/components/entry/Welcome.vue";
+import {mount} from '@vue/test-utils'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// https://pinia.vuejs.org/cookbook/testing.html
+import {createTestingPinia} from '@pinia/testing'
+import Welcome from '@/components/entry/Welcome.vue'
 
 const vuetify = createVuetify({
     components,
     directives,
-  });
+})
 
 test('contains proceed button', () => {
     const wrapper = mount(Welcome, {
@@ -17,5 +17,5 @@ test('contains proceed button', () => {
             plugins: [vuetify, createTestingPinia()],
         },
     })
-    expect(wrapper.text()).toContain('PROCEED');
-});
+    expect(wrapper.text()).toContain('PROCEED')
+})
