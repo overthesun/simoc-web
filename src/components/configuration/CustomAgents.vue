dba1a259b16a44d95e9470154b1589676546e60e
GregRRoss <grross1@asu.edu>
2024-07-22 00:43:34 -0700

    Fix linter issues

--- src/components/configuration/CustomAgents.vue
+++ src/components/configuration/CustomAgents.vue
@@ -2,40 +2,40 @@
 have been defined in local storage that should be present in the actual simulation -->
 
 <script>
-    export default {
-        data() {
-            return {
-                customAgents: []
-            }
-        },
-        async created() {
-            // Get currencies from simoc-abm
-            try {
-                // Get currencies from local storage.
-                //this.getLocalStorageCurrencies()
-                this.getLocalStorageAgents()
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        methods: {
-            getLocalStorageAgents() {
-                let localStorageAgents = localStorage.getItem('customAgents')
-                localStorageAgents = JSON.parse(localStorageAgents)
-                const localAgentNames = Object.keys(localStorageAgents)
-                this.customAgents = Object.keys(localStorageAgents)
-                if (typeof (localStorageAgents)==='object' && (localStorageAgents!==null)) {
-                    console.log('Custom agents found in local storage:')
-                    for (let index=0; index<localAgentNames.length; ++index) {
-                        // Modify this code such that it only adds an agent if it is NOT a plant
-                        console.log(localAgentNames[index])
-                    }
-                } else {
-                    console.log(' * No agents loaded from local storage *')
+export default {
+    data() {
+        return {
+            customAgents: [],
+        }
+    },
+    async created() {
+        // Get currencies from simoc-abm
+        try {
+            // Get currencies from local storage.
+            // this.getLocalStorageCurrencies()
+            this.getLocalStorageAgents()
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    methods: {
+        getLocalStorageAgents() {
+            let localStorageAgents = localStorage.getItem('customAgents')
+            localStorageAgents = JSON.parse(localStorageAgents)
+            const localAgentNames = Object.keys(localStorageAgents)
+            this.customAgents = Object.keys(localStorageAgents)
+            if (typeof (localStorageAgents)==='object' && (localStorageAgents!==null)) {
+                console.log('Custom agents found in local storage:')
+                for (let index=0; index<localAgentNames.length; ++index) {
+                    // Modify this code such that it only adds an agent if it is NOT a plant
+                    console.log(localAgentNames[index])
                 }
-            },
+            } else {
+                console.log(' * No agents loaded from local storage *')
+            }
         },
-    }
+    },
+}
 </script>
 <template>
     <div>
@@ -44,9 +44,9 @@ have been defined in local storage that should be present in the actual simulati
         <p>Custom Defined Agents other than plants.</p>
         <p style="color:red">THIS SECTION SHOULD ONLY APPEAR IF ANY AGENTS ARE IN LIST</p>
         <p style="color:red">REPLACE WITH SELECT LIKE PLANT SPECIES COMPONENT</p>
-        <div v-for="agent in this.customAgents">
+        <div v-for="agent in customAgents" :key="agent">
             <b>{{agent}}</b> Quantity:
-            <input type="number" value ="1"/>
+            <input type="number" value="1">
         </div>
     </div>
 </template>
