e72dbf494c4a9a117475ee27a86d6d363b40df63
Ezio Melotti <ezio.melotti@gmail.com>
2025-06-04 05:15:17 +0200

    Bump eslint-plugin-vue from 9.32.0 to 10.1.0 (#1068)
    * Bump eslint-plugin-vue from 9.32.0 to 10.1.0
    
    * Add the globals package to handle browser globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,18 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import vitest from '@vitest/eslint-plugin'
+import globals from 'globals'
 
 
 export default [
     js.configs.recommended,
     ...pluginVue.configs['flat/recommended'],
     {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
         rules: {
 
             // SPACING
