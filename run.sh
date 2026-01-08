b07354f79070df2fa225b401719deaac894ee643
Ezio Melotti <ezio.melotti@gmail.com>
2026-01-08 16:12:26 +0100

    Remove -c since it might conflict with --timestamping

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ fi
 
 echo '* Updating/downloading simdata/models/skybox files...'
 cd public
-wget -c -r -q --timestamping -erobots=off -nH --cut-dirs=1 --no-parent \
+wget -r -q --timestamping -erobots=off -nH --cut-dirs=1 --no-parent \
     --accept="*.gz" \
     https://simoc.space/download/simdata/
 echo '* Simdata files downloaded/updated in <public/simdata>'
 cd ..
 cd src/assets
-wget -c -r -q --timestamping -erobots=off -nH --cut-dirs=1 --no-parent \
+wget -r -q --timestamping -erobots=off -nH --cut-dirs=1 --no-parent \
     --accept="*.glb,*.jpg" \
     https://simoc.space/download/models/ \
     https://simoc.space/download/skybox/
